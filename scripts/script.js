0d56990afc6b70076f78ce7f006de61ba7cf844f
Камила Потеева <ares909@yandex.ru>
2020-10-23 15:30:57 +0300

    With form

--- scripts/script.js
+++ scripts/script.js
@@ -14,31 +14,22 @@ class PageChanger {
     document.querySelector(selectorToChange).style[styleToChange] = value;
   }
 }
-
+const DEFAULT_FONT = 'Arial';
+const VARIANTS = {
+	roboto: "Roboto",
+	kronaone: "Krona One",
+	oswald: "Oswald"
+}
 
 const page = new PageChanger(document.querySelector('.form__admin'));
 
 document.querySelector('.form__admin').addEventListener('submit', function (e) {
   e.preventDefault();
+  const targetFont = VARIANTS[page.getValue('font-family')] || DEFAULT_FONT
 
 
 
-  let fontFamilyToApply;
-
-  switch (page.getValue('font-family')) {
-    case 'roboto':
-      fontFamilyToApply = 'Roboto';
-      break;
-    case 'kronaone':
-      fontFamilyToApply = 'Krona One';
-      break;
-    case 'oswald':
-      fontFamilyToApply = 'Oswald';
-      break;
-    default:
-      fontFamilyToApply = 'Arial';
-  }
-  page.changeStyle('.content', 'font-family', fontFamilyToApply);
+  page.changeStyle('.content', 'font-family', targetFont);
 })
 document.querySelector('.form__toggle').onclick = () => {
   document.querySelector('.form').classList.toggle('form_is-opened');
